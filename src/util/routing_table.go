01b5823dcbd4b9744a9848a510d1acf3a5bb8050
JerseysGet <dev.faisal2004@gmail.com>
2025-04-12 20:36:51 +0530

    init routing table with -1

--- src/util/routing_table.go
+++ src/util/routing_table.go
@@ -8,6 +8,9 @@ func NewRoutingTable() *RoutingTable {
 	rt := make([][]int32, DIGITS)
 	for i := 0; i < DIGITS; i++ {
 		rt[i] = make([]int32, RADIX)
+		for j := range(rt[i]) {
+			rt[i][j] = -1
+		}
 	}
 	return &RoutingTable{
 		table: rt,
