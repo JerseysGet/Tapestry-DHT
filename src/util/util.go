6258fa739e94df7e0e22eb3871304a6cfd264afc
JerseysGet <dev.faisal2004@gmail.com>
2025-04-12 15:28:09 +0530

    src and util init

---
+++ src/util/util.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"math/bits"
+	"unsafe"
+)
+
+type hash_t uint64
+const RADIX = 4
+var DIGIT_SHIFT = bits.TrailingZeros(RADIX)
+var DIGIT_MASK = (1 << DIGIT_SHIFT) - 1;
+const DIGITS = 8 * unsafe.Sizeof(hash_t(0)) / RADIX
+
+func main() {
+	log.Printf("RADIX= %d", RADIX)	
+	log.Printf("DIGIT_SHIFT= %d", DIGIT_SHIFT)	
+	log.Printf("DIGIT_MASK= %d", DIGIT_MASK)	
+	log.Printf("DIGITS= %d", DIGITS)	
+
+}
