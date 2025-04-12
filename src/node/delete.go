5b6fe0537b9b7a2c1f745aa8e15b786c54e29a14
The-Broken-Keyboard <saurabhshreyansh868@gmail.com>
2025-04-12 23:20:56 +0530

    wrote rpc BPRemove

--- src/node/delete.go
+++ src/node/delete.go
@@ -1,3 +1,15 @@
 package main
 
-func (n *node) 
+func (n *node) BPRemove(ctx context.Context, req *pb.BPRemoveRequest) (*pb.BPRemoveResponse, error) {
+
+	port := int(req.Port)
+	
+	if _, exists := n.BP.Set[port]; exists {
+		delete(n.BP.Set, port)
+	} else {
+		return &pb.BPRemoveResponse{Success: false}, nil
+	}
+
+	return &pb.BPRemoveResponse{Success: true}, nil
+}
+
